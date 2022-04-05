b05c311f7f2850fbdf926e739f90571fadbfef71
Joao Fontes <joaoarnaldo1@hotmail.com>
2022-04-05 14:33:40 -0300

    Sistema Cadastro

---
+++ Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }    
+        public string DataNascimento { get; set; }
+        public string EstadoCivil { get; set; } 
+        public string Telefone { get; set; }    
+        public char sexo { get; set; }  
+
+    }
+}
